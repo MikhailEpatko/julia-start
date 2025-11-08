85541299709cc4fc5f42ebc36d9b3d86f040b7d5
Mikhail Epatko <m.epatko@jsa-group.ru>
2025-11-08 22:00:37 +0300

    issue 3

--- src/main/kotlin/ru/julia/start/Main.kt
+++ src/main/kotlin/ru/julia/start/Main.kt
@@ -1,5 +1,13 @@
 package ru.julia.start
 
 fun main() {
-    println("Hello, world!")
+    val a: Int = 1_000
+    val b = "log message"
+    val c = 3.14
+    val d = 100_000_000_000_000
+    val e = false
+    val f = '\n'
+    val g = 0
 }
+// Нужно явно указать правильный тип для переменных.
+// Пример того, как это правильно сделать: val a: Int = 1000
