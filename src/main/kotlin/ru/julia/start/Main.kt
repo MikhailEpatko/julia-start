43888fea4f6367e92882f7ac647594a22af7e454
Julia Start <ya_fm@mail.ru>
2025-11-09 22:20:37 +0500

    Задача 3

--- src/main/kotlin/ru/julia/start/Main.kt
+++ src/main/kotlin/ru/julia/start/Main.kt
@@ -1,11 +1,18 @@
 package ru.julia.start
 
 fun main() {
-    val a: Int = 1_000
-    val b = "log message"
-
+    val a: Int = 1000
+    val b: String = "log message"
+    val c: Double = 3.14
+    val d: Long = 100_000_000_000_000
+    val e: Boolean = false
+    val f: Char = '\n'
+    val g: Int = 0
     println(a)
     println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+    println(g)
 }
-// Нужно явно указать правильный тип для переменных.
-// Пример того, как это правильно сделать: val a: Int = 1000
