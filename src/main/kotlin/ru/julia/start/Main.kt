38f0fea652dbeab26691c299cb35da237ae88731
yafm-source <ya_fm@mail.ru>
2025-11-10 20:03:52 +0500

    Задача 8 when else

--- src/main/kotlin/ru/julia/start/Main.kt
+++ src/main/kotlin/ru/julia/start/Main.kt
@@ -1,18 +1,12 @@
 package ru.julia.start
 
 fun main() {
-    val a: Int = 1000
-    val b: String = "log message"
-    val c: Double = 3.14
-    val d: Long = 100_000_000_000_000
-    val e: Boolean = false
-    val f: Char = '\n'
-    val g: Int = 0
-    println(a)
-    println(b)
-    println(c)
-    println(d)
-    println(e)
-    println(f)
-    println(g)
+    var pizzaSlices = 0
+    pizzaSlices++
+    while (pizzaSlices < 8) {
+        println("There's only $pizzaSlices slice/s of pizza :(")
+        pizzaSlices++
+    }
+
+    println("There are $pizzaSlices slices of pizza. Hooray! We have a whole pizza! :D")
 }
