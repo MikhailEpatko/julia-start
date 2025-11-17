e42fc94ad7030f3495be9938367650c97e030480
Julia Start <ya_fm@mail.ru>
2025-11-17 11:01:47 +0500

    Задача 8 when else

--- src/main/kotlin/ru/julia/start/Main.kt
+++ src/main/kotlin/ru/julia/start/Main.kt
@@ -1,25 +1,12 @@
 package ru.julia.start
 
 fun main() {
-    val alphabets=("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
-    val button = alphabets
-    if (button == "A") {
-        println("Yes")
-    } else if (button == "B") {
-        println("No")
-    } else if (button == "X") {
-        println("Menu")
-    } else if (button == "Y") {
-        println("Nothing")
-    } else {
-        println("Error: There is no such button")
+    val button = "A"
+    when {
+        button == "A" -> println("Yes")
+        button == "B" -> println("No")
+        button == "X" -> println("Menu")
+        button == "Y" -> println("Nothing")
+        else -> print("Error: There is no such button")
     }
-//button - это кнопка, которую пользователь нажал на контроллере (джойстике, клавиатуре).
-//По нажатию на кнопку, код должен выводить в консоль соответствующий ей ответ или ошибку, если драйвер про такую кнопку ничего не знает.
-//
-//Кнопка - Ответ
-//A - "Yes"
-//B - "No"
-//X - "Menu"
-//Y - "Nothing"
-//Любая другая - "Error: There is no such button"
+}
