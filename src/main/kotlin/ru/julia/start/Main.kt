f5d0b206c4e1462cfa891b1c239a64fdc172a016
Julia Start <ya_fm@mail.ru>
2025-11-19 01:54:24 +0500

    Задача 10 do while

--- src/main/kotlin/ru/julia/start/Main.kt
+++ src/main/kotlin/ru/julia/start/Main.kt
@@ -1,12 +1,8 @@
 package ru.julia.start
 
 fun main() {
-    val name = "Marry"
-    val age = 20
-    
-    val name2 = "My name is $name. And I'm $age years old"
-    println(name2)
+//    val numb =1..100
+    for (numb in 1..100 step 3) {
+        println(numb)
+    }
 }
-// Нужно дописать код, чтобы он выводил в консоль:
-// My name is Marry. And I'm 20 years old.
-// Подсказка: код должен подставлять значения переменных name и age в шаблон строки.
