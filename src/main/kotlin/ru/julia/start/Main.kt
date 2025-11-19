c1d8cf75b703873d46569c97630bbd1f669b86b9
Mikhail <MikhailEpatko@users.noreply.github.com>
2025-11-19 09:14:56 +0300

    Remove files from cache (#36)
    * ttt
    
    * ttt
    
    * removed files from cache
    
    ---------
    
    Co-authored-by: Mikhail Epatko <m.epatko@jsa-group.ru>

--- src/main/kotlin/ru/julia/start/Main.kt
+++ src/main/kotlin/ru/julia/start/Main.kt
@@ -1,13 +1,5 @@
 package ru.julia.start
 
-import kotlin.random.Random
-
 fun main() {
-    val firstResult = Random.nextInt(6)
-    val secondResult = Random.nextInt(6)
-    if (firstResult == secondResult) {
-        println("Вы победили!")
-    } else {
-        println("Не в этот раз...")
-    }
+    println("main")
 }
