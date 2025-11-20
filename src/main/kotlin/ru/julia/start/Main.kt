933137612be25f4a5cc280270c01cfd460a1642c
Julia Start <ya_fm@mail.ru>
2025-11-20 08:13:52 +0500

    Задача 11

--- src/main/kotlin/ru/julia/start/Main.kt
+++ src/main/kotlin/ru/julia/start/Main.kt
@@ -4,9 +4,9 @@ fun main() {
     val numb = 1..100
     for (number in numb) {
         when {
+            number % (3 * 5) == 0 -> println("fizzbuzz")
             number % 3 == 0 -> println("fizz")
             number % 5 == 0 -> println("buzz")
-            number % 3 % 5 == 0 -> println("fizzbuzz")
         }
     }
 }
