5c0a27d09fcb4977b795be5389ce12fb9b221edf
Julia Start <ya_fm@mail.ru>
2025-11-22 20:05:04 +0500

    Задача 12 filterIndexed

--- src/main/kotlin/ru/julia/start/Main.kt
+++ src/main/kotlin/ru/julia/start/Main.kt
@@ -1,11 +1,10 @@
 package ru.julia.start
 
 fun main() {
-//    val words = listOf("dinosaur", "limousine", "magazine", "language")
-//    val letter = words.filter { words -> words.contains("l") }
-//    println(letter)
     val words = listOf("dinosaur", "limousine", "magazine", "language")
-    val res = words.filterIndexed { index, value -> index != 0 && value.startsWith('l') }
-//        (words[0].startsWith("l"))
-    println(res)
+    for (word in words) {
+        if (word.startsWith("l")) {
+            println(word)
+        }
+    }
 }
