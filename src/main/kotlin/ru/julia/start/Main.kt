d0990813f9e0e266cc3cff2942141e4e89e746b7
Julia Start <ya_fm@mail.ru>
2025-11-25 17:39:01 +0500

    Changes

--- src/main/kotlin/ru/julia/start/Main.kt
+++ src/main/kotlin/ru/julia/start/Main.kt
@@ -1,11 +1,11 @@
 package ru.julia.start
 
 fun main() {
-         for (i in 1..5) {
-        for (y in 1..5-i) {
+    for (i in 1..5) {
+        for (y in 1..5 - i) {
             print(" ")
         }
-        for (k in 1..2*i-1){
+        for (k in 1..2 * i - 1) {
             print("*")
         }
         println()
