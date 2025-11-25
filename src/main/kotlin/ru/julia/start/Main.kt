37d462e4573087b56762ac1a478c13034d727133
Julia Start <ya_fm@mail.ru>
2025-11-25 21:58:40 +0500

    12.4

--- src/main/kotlin/ru/julia/start/Main.kt
+++ src/main/kotlin/ru/julia/start/Main.kt
@@ -1,5 +1,25 @@
 package ru.julia.start
 
 fun main() {
-    println("main")
+    val n = 5
+
+    for (i in 1..n) {
+        for (y in 1..n - i) {
+            print(" ")
+        }
+        for (r in 1..i) {
+            print("* ")
+        }
+        println()
+    }
+    val m = 5
+    for (l in 4 downTo 1){
+        for (t in m - l downTo 1){
+            print(" ")
+        }
+        for (s in l downTo 1 ){
+            print("* ")
+    }
+        println()
 }
+    }
