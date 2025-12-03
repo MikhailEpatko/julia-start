fc46dedfd0bff6b31b879c72e90fb794002a5ec3
Mikhail <MikhailEpatko@users.noreply.github.com>
2025-12-03 19:13:12 +0300

    Lesson (#60)
     updated .eitorconfig

---
+++ src/test/kotlin/ru/julia/start/MainTest.kt
@@ -0,0 +1,14 @@
+package ru.julia.start
+
+import org.junit.jupiter.api.DisplayName
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class MainTest {
+
+    @Test
+    @DisplayName("Проверяем выполнение платежа и возврат средств")
+    fun payAndRefundTest() {
+        assertTrue { true }
+    }
+}
