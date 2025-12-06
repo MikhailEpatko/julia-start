9aebe838a7f49d6e26a8758e312410277579b701
Julia Start <ya_fm@mail.ru>
2025-12-06 22:59:48 +0500

    13

--- src/main/kotlin/ru/julia/start/Main.kt
+++ src/main/kotlin/ru/julia/start/Main.kt
@@ -6,14 +6,11 @@ fun main() {
     intervalInSeconds(1, 20, 15)
     intervalInSeconds(minutes = 1, seconds = 25)
     intervalInSeconds(2)
-    intervalInSeconds(minutes= 10)
-    intervalInSeconds(hours=1,seconds= 1)
+    intervalInSeconds(minutes = 10)
+    intervalInSeconds(hours = 1, seconds = 1)
 }
 
-fun intervalInSeconds(
-    hours: Int = 0
-    minutes: Int =0
-    seconds: Int = 0
-)Int=(hours * 60) + minutes) * 60 + seconds){
-    println(intervalInSeconds())
+fun intervalInSeconds(hours: Int = 0, minutes: Int = 0, seconds: Int = 0) {
+    val res = (hours * 60 + minutes) * 60 + seconds
+    println(res)
 }
