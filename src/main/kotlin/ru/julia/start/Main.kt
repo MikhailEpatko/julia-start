80fe5f5254a54c4d14f962f6ce92d579e514e2cf
Julia Start <ya_fm@mail.ru>
2025-12-06 16:38:31 +0500

    13

--- src/main/kotlin/ru/julia/start/Main.kt
+++ src/main/kotlin/ru/julia/start/Main.kt
@@ -1,5 +1,21 @@
 package ru.julia.start
 
+import kotlin.time.Duration.Companion.hours
+
 fun main() {
-    println("main")
+    intervalInSeconds(1, 20, 15)
+    intervalInSeconds(minutes = 1, seconds = 25)
+    println(intervalInSeconds(2))
+    println(intervalInSeconds(minutes= 10, 0)
+    println(intervalInSeconds(houers=1,seconds= 1)
 }
+
+fun intervalInSeconds(
+    hours: Int = 0
+    minutes: Int =0
+    seconds: Int = 0
+){
+    println((hours * 60) + minutes) * 60 + seconds))
+
+}
+    (
