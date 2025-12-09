cc9daa2eded8c9a2eade66bd80b32983b6c6c7a7
Julia Start <ya_fm@mail.ru>
2025-12-09 19:35:25 +0500

    16

--- src/main/kotlin/ru/julia/start/Main.kt
+++ src/main/kotlin/ru/julia/start/Main.kt
@@ -4,7 +4,10 @@ fun createUrl(
     prefix: String="https://example.com/book-info",
     id:Int =5,
     actions: List<String> = listOf("title", "year", "author"),
-):{
+): {
+    val res = "$prefix/$id/$actions"
+    println(res)
 
 }
+
 
