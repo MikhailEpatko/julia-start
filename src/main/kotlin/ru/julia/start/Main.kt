4151b3c1661a29c29c1583c2bf8d5515091bf36d
Julia Start <ya_fm@mail.ru>
2025-12-09 19:56:14 +0500

    16

--- src/main/kotlin/ru/julia/start/Main.kt
+++ src/main/kotlin/ru/julia/start/Main.kt
@@ -1,13 +1,16 @@
 package ru.julia.start
 
-fun createUrl(
-    prefix: String="https://example.com/book-info",
-    id:Int =5,
-    actions: List<String> = listOf("title", "year", "author"),
-): {
-    val res = "$prefix/$id/$actions"
-    println(res)
+import jdk.javadoc.internal.doclets.formats.html.markup.HtmlStyle
 
+fun main() {
+    createUrl("https://example.com/book-info", 5, "title")
 }
 
-
+fun createUrl(
+    prefix: String = "https://example.com/book-info",
+    id: Int = 5,
+    actions: List<String> = listOf("title", "year", "author"),
+) {
+    val res = "$prefix/$id/$actions"
+    println(res)
+}
