f8f7d1fb29c5b0f5b39c1b8c6687b38fb77e49cb
Julia Start <ya_fm@mail.ru>
2025-12-11 21:12:14 +0500

    fixup! 16

--- src/main/kotlin/ru/julia/start/Main.kt
+++ src/main/kotlin/ru/julia/start/Main.kt
@@ -1,16 +1,1 @@
 package ru.julia.start
-
-fun main() {
-    println(createUrl(actions = listOf("title")))
-    println(createUrl(actions = listOf("year")))
-    println(createUrl(actions = listOf("author")))
-}
-
-fun createUrl(
-    prefix: String = "https://example.com/book-info",
-    id: Int = 5,
-    actions: List<String> = listOf("title", "year", "author"),
-): List<String> {
-    val res = "$prefix/$id/$actions"
-    return res
-}
