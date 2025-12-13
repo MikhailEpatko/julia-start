7178477766dd8734915cb202ee9bec0149b5efc9
Julia Start <ya_fm@mail.ru>
2025-12-13 20:00:55 +0500

    16

--- src/main/kotlin/ru/julia/start/Main.kt
+++ src/main/kotlin/ru/julia/start/Main.kt
@@ -1,5 +1,12 @@
 package ru.julia.start
 
 fun main() {
-    println("main")
+    // Write your code here
+}
+
+fun repeatN(
+    n: Int,
+    action: () -> Unit,
+) {
+    // Write your code here
 }
