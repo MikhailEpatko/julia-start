12c765049812707176a47bcb970f6ddd64a041fd
Julia Start <ya_fm@mail.ru>
2025-12-20 20:09:44 +0500

    64

--- src/main/kotlin/ru/julia/start/Main.kt
+++ src/main/kotlin/ru/julia/start/Main.kt
@@ -1,5 +1,11 @@
 package ru.julia.start
 
+fun main() {
+    println(listOf<Int>(0, 1, 2, 3, 4))
+    println(listOf<Int>(0))
+    println(listOf<Int>(1, 3))
+}
+
 fun evenNumberCount(numbers: List<Int>): Int {
     for (a in 0..4) {
         if (a % 2 == 0) {
