a414df8393bed9c3998034e2329814b941d31e62
Julia Start <ya_fm@mail.ru>
2025-12-22 21:50:21 +0500

    65

--- src/main/kotlin/ru/julia/start/Main.kt
+++ src/main/kotlin/ru/julia/start/Main.kt
@@ -1,14 +1,17 @@
 package ru.julia.start
 
 fun main() {
-    println(listone)
+    println(separateNumbers(listOf(0, 4, 3, 3, 1, 6, 5, 0, 2)))
+    println(separateNumbers(listOf(0)))
+    println(separateNumbers(listOf(3, 1)))
+    println(separateNumbers(listOf(1, 2)))
 }
 
 fun separateNumbers(numbers: List<Int>): List<Int> {
     val listone = mutableListOf<Int>()
     val listtwo = mutableListOf<Int>()
     for (number in numbers) {
-        if (number % 2 == 0) {
+        if (number % 2 == 0 && ) {
             listone.add(number)
         } else {
             listtwo.add(number)
