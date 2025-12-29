2e431ca015eb46c7a1399c9cea1732b251f75052
Mikhail <MikhailEpatko@users.noreply.github.com>
2025-12-29 23:25:17 +0300

    Update Main.kt

--- src/main/kotlin/ru/julia/start/Main.kt
+++ src/main/kotlin/ru/julia/start/Main.kt
@@ -1,20 +1,7 @@
 package ru.julia.start
 
 fun main() {
-    println(separateNumbers(listOf(0, 4, -3, 3, 1, -6, -5, 0, -2)))
-    println(separateNumbers(listOf(0)))
-    println(separateNumbers(listOf(-3, 1)))
-    println(separateNumbers(listOf(1, -2)))
+    println("main")
 }
 
-fun separateNumbers(numbers: List<Int>): List<Int> {
-    val listone = mutableListOf<Int>()
-    for (number in numbers) {
-        if (number < 0) {
-            listone.add(-number)
-        } else {
-            listone.add(number)
-        }
-    }
-    return listone
-}
+fun makePositive(numbers: List<Int>): List<Int> = numbers.map { if (it < 0) -it else it }
