1ac268d6e800d58b8188498ea9f05aad38ebdf13
Mikhail Epatko <m.epatko@jsa-group.ru>
2025-12-30 00:03:00 +0300

    updated project configs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,38 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val ktlint: Configuration by configurations.creating
+
 plugins {
-    kotlin("jvm") version "2.0.21"
-    id("io.gitlab.arturbosch.detekt") version "1.23.8"
-    id("org.jlleitschuh.gradle.ktlint") version "13.1.0"
+    kotlin("jvm") version "2.2.20"
+    id("dev.detekt") version "2.0.0-alpha.1"
+    id("jvm-toolchains")
+    id("java")
+}
+
+develocity {
+    buildScan {
+        termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
+        termsOfUseAgree.set("no")
+        publishing.onlyIf { false }
+    }
+}
+
+configurations {
+    ktlint
 }
 
 detekt {
+    toolVersion = "2.0.0-alpha.1"
+    config.setFrom(file("detekt/config.yml"))
+    buildUponDefaultConfig = true
     source.setFrom("src/main/kotlin")
-    config.setFrom("detekt/config.yml")
-    buildUponDefaultConfig = false
     parallel = true
-    basePath = projectDir.path
-}
-
-ktlint {
-    android = false
-    ignoreFailures = false
 }
 
 group = "ru.julia.start"
 version = "0.0.0"
+java.sourceCompatibility = JavaVersion.VERSION_21
 
 repositories {
     mavenCentral()
@@ -30,14 +41,34 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+
+    ktlint("com.pinterest.ktlint:ktlint-cli:1.8.0") {
+        attributes {
+            attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.EXTERNAL))
+        }
+    }
 }
 
 tasks.test {
     useJUnitPlatform()
 }
 
+val ktlintCheck by tasks.registering(JavaExec::class) {
+    group = LifecycleBasePlugin.VERIFICATION_GROUP
+    description = "Check Kotlin code style"
+    classpath = ktlint
+    mainClass.set("com.pinterest.ktlint.Main")
+    // see https://pinterest.github.io/ktlint/install/cli/#command-line-usage for more information
+    args(
+        "-F",
+        "**/src/**/*.kt",
+        "**.kts",
+        "!**/build/**",
+    )
+}
+
 tasks.detekt {
-    dependsOn(tasks.ktlintCheck)
+    dependsOn(ktlintCheck)
 }
 
 tasks.withType<KotlinCompile> {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,5 @@
 rootProject.name = "julia-start"
+
+plugins {
+    id("com.gradle.develocity") version "4.3"
+}
