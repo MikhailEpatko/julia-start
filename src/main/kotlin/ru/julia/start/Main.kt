f8492676cc1a682722c72dcd653178b6ca7853bb
Julia Start <ya_fm@mail.ru>
2026-01-07 21:16:49 +0500

    45 flipNumber

--- src/main/kotlin/ru/julia/start/Main.kt
+++ src/main/kotlin/ru/julia/start/Main.kt
@@ -14,7 +14,7 @@ fun flipTheNumber(input: Int): Int {
     var lastNumber = 0
 
     while (number > 0) {
-        lastNumber = (lastNumber * 10) + (number % 10)
+        lastNumber = lastNumber * 10 + number % 10
         number /= 10
     }
     return lastNumber
--- src/test/kotlin/ru/julia/start/MainTest.kt
+++ src/test/kotlin/ru/julia/start/MainTest.kt
@@ -1,14 +1,28 @@
 package ru.julia.start
 
+import org.junit.jupiter.api.Assertions.assertAll
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.function.Executable
 import kotlin.test.Test
-import kotlin.test.assertTrue
+import kotlin.test.assertEquals
 
 class MainTest {
 
     @Test
-    @DisplayName("Проверяем выполнение платежа и возврат средств")
-    fun payAndRefundTest() {
-        assertTrue { true }
+    @DisplayName("Тест переворачивания числа")
+    fun flipTheNumberTest() {
+        val testData = mapOf(
+            123 to 321,
+            322 to 223,
+            1 to 1,
+            0 to 0,
+            3412 to 2143,
+            701118291 to 192811107,
+        )
+        assertAll(
+            testData.map { (input, expected) ->
+                Executable { assertEquals(expected, flipTheNumber(input), "число: $input") }
+            }
+        )
     }
 }
