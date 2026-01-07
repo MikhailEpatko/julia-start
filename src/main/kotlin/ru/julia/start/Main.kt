ec9e255a75667f1d7ad2c05629616ab9415b875d
Julia Start <ya_fm@mail.ru>
2026-01-07 16:43:30 +0500

    45 flipNumber

--- src/main/kotlin/ru/julia/start/Main.kt
+++ src/main/kotlin/ru/julia/start/Main.kt
@@ -1,6 +1,12 @@
 package ru.julia.start
 
 fun main() {
+    println("123->${flipTheNumber(123)}")
+    println("322)->${flipTheNumber(322)"}
+    println(1)
+    println(0)
+    println(3412)
+    println(701118291)
 }
 
 fun flipTheNumber(input: Int): Int {
@@ -10,6 +16,6 @@ fun flipTheNumber(input: Int): Int {
     if (number > 0) {
         lastNumber = (lastNumber * 10) + (number % 10)
         number /= 10
-    } else {
     }
+    return if (number < 0) -lastNumber else lastNumber
 }
