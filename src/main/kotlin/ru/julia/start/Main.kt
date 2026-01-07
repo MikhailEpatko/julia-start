aba9c8e8d949fbf1f2affe1a5d41c333b40cfaa7
Julia Start <ya_fm@mail.ru>
2026-01-07 16:25:05 +0500

    66 -to+

--- src/main/kotlin/ru/julia/start/Main.kt
+++ src/main/kotlin/ru/julia/start/Main.kt
@@ -4,4 +4,12 @@ fun main() {
 }
 
 fun flipTheNumber(input: Int): Int {
+    var number = readln().toInt()
+    var lastNumber = 0
+
+    if (number > 0) {
+        lastNumber = lastNumber * 10 + (number % 10)
+        number /= 10
+    } else {
+    }
 }
