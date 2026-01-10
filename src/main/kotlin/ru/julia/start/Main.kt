f865669a07876295359bfcf6a8bad0affc159d8d
Julia Start <ya_fm@mail.ru>
2026-01-10 15:43:19 +0500

    46 countWords

--- src/main/kotlin/ru/julia/start/Main.kt
+++ src/main/kotlin/ru/julia/start/Main.kt
@@ -3,3 +3,26 @@ package ru.julia.start
 fun main() {
     println("main")
 }
+
+fun countWords(input: String): Int {
+    if (input.isEmpty()) {
+        return 0
+    }
+
+    var counter = 0
+    var inWord = false
+    for (char in input) {
+        when (char) {
+            ' ', '\n', '\t' -> {
+                if (inWord) {
+                    counter++
+                }
+                inWord = false
+            }
+        } else ->{
+            if(!inWord){
+                inWord = true
+            }
+        }
+    }
+}
