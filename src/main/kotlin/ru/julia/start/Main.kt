047c9a57adc7b16e325fd7ba0ecdc021d9d639d8
Mikhail Epatko <m.epatko@jsa-group.ru>
2026-01-10 19:42:55 +0300

    added example

--- src/main/kotlin/ru/julia/start/Main.kt
+++ src/main/kotlin/ru/julia/start/Main.kt
@@ -8,21 +8,20 @@ fun countWords(input: String): Int {
     if (input.isEmpty()) {
         return 0
     }
-
     var counter = 0
     var inWord = false
     for (char in input) {
         when (char) {
-            ' ', '\n', '\t' -> {
+            ' ', '\n', '\t' ->
                 if (inWord) {
                     counter++
                     inWord = false
                 }
-            } else -> {
+
+            else ->
                 if (!inWord) {
                     inWord = true
                 }
-            }
         }
     }
     if (inWord) {
@@ -30,3 +29,15 @@ fun countWords(input: String): Int {
     }
     return counter
 }
+
+fun countWords2(input: String): Int =
+    input
+        .split("\\s+".toRegex()) // 1
+        .count { !it.isEmpty() } // 2
+
+// 1 - разделяем строку на список строк по шаблону-разделителя.
+//     В данном случае шаблон разделителя опеределяется как регулярное выражение
+//     "//s+".toRegex(), которое читается как "любое количество пустых символов
+//     (пробел, табуляция, переход на новую строку и др.).
+//
+// 2 - считаем количество элементов сеписка, удовлетворяющих условию "не пустая строка".
